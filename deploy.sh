0f1e554cd2dd6957d837c7d3fc06aa6061c433a4
boopesh3737 <127740144+boopesh3737@users.noreply.github.com>
2025-02-05 10:06:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u boopeshspidy -p boopesh37
-    docker tag test boopeshspidy/task2
-    docker push boopeshspidy/task2
+    docker login -u boopeshs -p 02-Apr-05
+    docker tag test boopeshs/task2
+    docker push boopeshs/task2
     
