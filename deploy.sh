6844a8047e653d76010d85630efde4ad60b639ee
boopesh3737 <127740144+boopesh3737@users.noreply.github.com>
2025-02-06 15:18:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@
     docker login -u boopeshs -p 02-Apr-05
     docker tag test boopeshs/task2
     docker push boopeshs/task2
-    
+    kubectl create deployment spidy7 --images=boopeshs/task2 --port=80
+    kubevtl expose deployment spidy7 --type=NodePort --port=80
